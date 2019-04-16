2d3351d1eccd970b131518bc7193d899d0fcc1f1
afurey <afurey@us.ibm.com>
2019-04-16 15:18:12 -0400

    Add files via upload

---
+++ capstone.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Apr 16 14:43:03 2019
+
+@author: AmyFurey
+"""
+#I am using Anaconda Spyder and their is no Markdown capabilities
+#This notebook will be primarily used for the capstone project.
+
+import pandas as pd
+import numpy as np
+print ("Hello Capstone Project Course!")
+
